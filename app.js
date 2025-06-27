34b3c8026d3f75e8c249a2996faacc7998c05a5f
Tabish46-coder <tabishhussain387@gmail.com>
2025-06-28 01:13:44 +0500

    deploy ho jana chahiya ab

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
-const loginroutes= require('./routes/loginRoutes')
+const loginroutes = require('./routes/loginRoutes');
 const notification = require('./routes/notificationRoutes');
 const tickets = require('./routes/ticketRoutes');
 const proof = require('./routes/proofRoutes');
 const workinstruction = require('./routes/workinstructionRouter');
-const management_wi= require('./routes/management_wi_routes')
+const management_wi = require('./routes/management_wi_routes');
+
 dotenv.config();
 connectDB();
 
@@ -26,7 +27,13 @@ app.get('/ping', (req, res) => {
   res.send('Server is alive!');
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
-});
+// Correct PORT usage
+const PORT = process.env.PORT;
+if (!PORT) {
+  console.error("❌ Railway PORT env variable is missing.");
+} else {
+  console.log(`✅ Using Railway PORT: ${PORT}`);
+  app.listen(PORT, () => {
+    console.log(`✅ Server is running on port ${PORT}`);
+  });
+}
--- config/db.js
+++ config/db.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
+const MONGO_URI = 'mongodb+srv://tabish_31:7cXVLESKNt5MLLsk@deskhelp.gri6dmp.mongodb.net/?retryWrites=true&w=majority&appName=Deskhelp';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(MONGO_URI);
     console.log('MongoDB Connected');
   } catch (err) {
     console.error(err);
     process.exit(1);
   }
 };
+
 
 module.exports = connectDB;
