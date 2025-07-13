b6451383bfab5dd87fb7bb243a69dbd688c9ff23
Tabish46-coder <tabishhussain387@gmail.com>
2025-07-13 23:34:26 +0500

    new test

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,16 +1,6 @@
 const admin = require('firebase-admin');
-
-const serviceAccount = JSON.parse(
-  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf8')
-);
-
-// Fix escaped newlines in private_key
-if (serviceAccount.private_key) {
-  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
-}
+const serviceAccount = require('../ht-helpdesk-firebase-adminsdk-fbsvc-8b64833108.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
-
-module.exports = admin;
